ad7d91106658cf92e8682184683665178ef2f2c7
DavidBetteridge <davidabetteridge@gmail.com>
2018-05-03 09:43:32 +0100

    Rewrite

---
+++ solutions/David_Maria_Max/Farkle/Rule_RollOf.cs
@@ -0,0 +1,37 @@
+﻿namespace Farkle
+{
+    public enum LotsOf
+    {
+        Ones = 1,
+        Twos = 2,
+        Threes = 3,
+        Fours = 4,
+        Fives = 5,
+        Sixes = 6
+    }
+
+    class Rule_RollOf
+    {
+        public int Quantity;
+        public int NumberOfSpots;
+        public int Score;
+
+        public Rule_RollOf(int quantity, LotsOf numberOfSpots, int Scores)
+        {
+            this.Quantity = quantity;
+            this.NumberOfSpots = (int)numberOfSpots;
+            this.Score = Scores;
+        }
+
+        public (int[] remainingDice, int score) Apply(int[] numberOf, int currentScore)
+        {
+            var totalScore = currentScore;
+            while (numberOf[this.NumberOfSpots] >= this.Quantity)
+            {
+                numberOf[this.NumberOfSpots] -= this.Quantity;
+                totalScore += this.Score;
+            }
+            return (numberOf, totalScore);
+        }
+    }
+}
---
+++ solutions/David_Maria_Max/Farkle/Scorer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Farkle
+{
+    public class Scorer
+    {
+        public int Evaluate(int[] diceRolls)
+        {
+            var rules = new List<Rule_RollOf>()
+            {
+                new Rule_RollOf(3,LotsOf.Ones,Scores:1000),
+                new Rule_RollOf(3,LotsOf.Twos,Scores:200),
+                new Rule_RollOf(3,LotsOf.Threes,Scores:300),
+                new Rule_RollOf(3,LotsOf.Fours,Scores:400),
+                new Rule_RollOf(3,LotsOf.Fives,Scores:500),
+                new Rule_RollOf(3,LotsOf.Sixes,Scores:600),
+                new Rule_RollOf(1,LotsOf.Ones,Scores:100),
+                new Rule_RollOf(1,LotsOf.Fives,Scores:50),
+            };
+
+            var numberOf = CountDice(diceRolls);
+            var initialState = (RemainingDice: numberOf, RunningTotal: 0);
+            var totalScore = rules.Aggregate(initialState,
+                                             (state, rule) => rule.Apply(state.RemainingDice, state.RunningTotal)).RunningTotal;
+            return totalScore;
+        }
+
+        private int[] CountDice(int[] diceRolls)
+            => Enumerable.Range(0, 7).Select(numberOfSpots => NumberOfDice(diceRolls, numberOfSpots)).ToArray();
+
+        private int NumberOfDice(int[] diceRolls, int numberOfSpots) => diceRolls.Count(roll => roll == numberOfSpots);
+    }
+}
